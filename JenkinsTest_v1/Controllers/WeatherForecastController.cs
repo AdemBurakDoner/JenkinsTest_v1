d01de2a13d11ef2ebb277d04e96bd9e17cd7207f
KOCYASA\ademburak.doner <ademburakdoner@gmail.com>
2023-02-01 13:33:24 +0300

    #
    controller update

--- JenkinsTest_v1/Controllers/WeatherForecastController.cs
+++ JenkinsTest_v1/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace JenkinsTest_v1.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                Ops = "Holaa!!!"
+                Ops = "Holaa!"
             })
             .ToArray();
         }
